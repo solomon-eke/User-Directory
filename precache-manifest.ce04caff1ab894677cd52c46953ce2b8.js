f231c35f11a1f5a6c6412a9bbdf644d72c607a71
Solomon <ekesolomon11@gmail.com>
2020-06-24 17:48:17 -0400

    Updates

---
+++ precache-manifest.ce04caff1ab894677cd52c46953ce2b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34cfd18eb87354f586bc46bdd26d18a3",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "8c348b9b8a1c7bc6a97a",
+    "url": "/User-Directory/static/css/main.268ef553.chunk.css"
+  },
+  {
+    "revision": "29a6451428f7846114fc",
+    "url": "/User-Directory/static/js/2.4e540142.chunk.js"
+  },
+  {
+    "revision": "0a572ad323474bafc6a871b6336141bd",
+    "url": "/User-Directory/static/js/2.4e540142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c348b9b8a1c7bc6a97a",
+    "url": "/User-Directory/static/js/main.4dcc7f49.chunk.js"
+  },
+  {
+    "revision": "e1c83858e31256f2d3c4",
+    "url": "/User-Directory/static/js/runtime-main.941ed641.js"
+  }
+]);
